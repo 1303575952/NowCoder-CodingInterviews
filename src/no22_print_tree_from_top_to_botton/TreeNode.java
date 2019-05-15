2585e4e7b6b9d13c0a8eb975a16ebb8c839f211b
1303575952@qq.com <1303575952@qq.com>
2019-05-16 07:52:23 +0800

    从上往下打印二叉树

---
+++ src/no22_print_tree_from_top_to_botton/TreeNode.java
@@ -0,0 +1,11 @@
+package no22_print_tree_from_top_to_botton;
+
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
