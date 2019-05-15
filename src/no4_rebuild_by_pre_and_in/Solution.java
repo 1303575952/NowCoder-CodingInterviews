083093e21193cafaff708e6550813dbdd7531a9b
1303575952@qq.com <1303575952@qq.com>
2019-05-15 19:38:46 +0800

    重建二叉树

---
+++ src/no4_rebuild_by_pre_and_in/Solution.java
@@ -0,0 +1,30 @@
+package no4_rebuild_by_pre_and_in;
+
+/**
+ * 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
+ * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ * 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
+ */
+public class Solution {
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        return reConstructBinaryTree(pre, 0, pre.length-1, in, 0, in.length-1);
+    }
+
+    public TreeNode reConstructBinaryTree(int[] pre, int preStart, int preEnd, int[] in, int inStart, int inEnd) {
+        if (preStart > preEnd || inStart > inEnd) {
+            return null;
+        }
+        TreeNode treeNode = new TreeNode(pre[preStart]);
+        for (int i = inStart; i <= inEnd; i++) {
+            if (in[i] == pre[preStart]) {
+                treeNode.left = reConstructBinaryTree(pre, preStart + 1, preStart + i - inStart, in, inStart, i - 1);
+                treeNode.right = reConstructBinaryTree(pre, preStart + i - inStart + 1, preEnd, in, i + 1, inEnd);
+            }
+        }
+        return treeNode;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/no4_rebuild_by_pre_and_in/TreeNode.java
@@ -0,0 +1,11 @@
+package no4_rebuild_by_pre_and_in;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
