d4174178df9581e83ddb1b76cd3ea294529ea4ea
1303575952@qq.com <1303575952@qq.com>
2019-05-15 23:33:01 +0800

    树的子结构，二叉树的镜像

---
+++ src/no17_sub_tree/Solution.java
@@ -0,0 +1,41 @@
+package no17_sub_tree;
+
+/**
+ * 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+ */
+public class Solution {
+
+    public static boolean isSubTree(TreeNode node1, TreeNode node2) {
+        if (node2 == null) {
+            return true;
+        }
+        if (node1 == null && node2 != null) {
+            return false;
+        }
+        if (node1.val == node2.val) {
+            return isSubTree(node1.left, node2.left) && isSubTree(node1.right, node2.right);
+        } else {
+            return false;
+        }
+    }
+
+    public boolean hasSubTree(TreeNode node1, TreeNode node2) {
+        if (node2 == null) {
+            return false;
+        }
+        if (node1 == null && node2 != null) {
+            return false;
+        }
+        boolean flag = false;
+        if (node1.val == node2.val) {
+            flag = isSubTree(node1, node2);
+        }
+        if (!flag) {
+            flag = hasSubTree(node1.left, node2);
+            if (!flag) {
+                flag = hasSubTree(node1.right, node2);
+            }
+        }
+        return flag;
+    }
+}
---
+++ src/no18_mirror_tree/TreeNode.java
@@ -0,0 +1,11 @@
+package no18_mirror_tree;
+
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
