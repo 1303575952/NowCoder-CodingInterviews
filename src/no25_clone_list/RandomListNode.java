529060cd59c4346ee8882faee2dabedc6c54b1a6
1303575952@qq.com <1303575952@qq.com>
2019-05-16 11:17:53 +0800

    复杂链表的复制

---
+++ src/no25_clone_list/RandomListNode.java
@@ -0,0 +1,11 @@
+package no25_clone_list;
+
+public class RandomListNode {
+	int label;
+	RandomListNode next = null;
+	RandomListNode random = null;
+
+	RandomListNode(int label) {
+		this.label = label;
+	}
+}
---
+++ src/no25_clone_list/Solution.java
@@ -0,0 +1,43 @@
+package no25_clone_list;
+
+/**
+ * 输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，另一个特殊指针指向任意一个节点），
+ * 返回结果为复制后复杂链表的head。
+ * （注意，输出结果中请不要返回参数中的节点引用，否则判题程序会直接返回空）
+ */
+public class Solution {
+    public RandomListNode cloneList(RandomListNode pHead) {
+        if (pHead == null) {
+            return null;
+        }
+
+        RandomListNode currentNode = pHead;
+        //根据原始链表的每个节点N创建对应节点N'，把N'链接在N后面
+        while (currentNode != null) {
+            RandomListNode cloneNode = new RandomListNode(currentNode.label);
+            RandomListNode nextNode = currentNode.next;
+            currentNode.next = cloneNode;
+            cloneNode.next = nextNode;
+            currentNode = nextNode;
+        }
+
+        currentNode = pHead;
+        //设置复制出来的节点的随机指针
+        while (currentNode != null) {
+            currentNode.next.random = currentNode.random == null ? null : currentNode.random.next;
+            currentNode = currentNode.next.next;
+        }
+
+        currentNode = pHead;
+        RandomListNode pCloneHead = pHead.next;
+        //拆分链表
+        while (currentNode != null) {
+            RandomListNode cloneNode = currentNode.next;
+            currentNode.next = cloneNode.next;
+            cloneNode.next = cloneNode.next == null ? null : cloneNode.next.next;
+            currentNode = currentNode.next;
+        }
+
+        return pCloneHead;
+    }
+}
