53d4336a583ead19679bcf5ad2300047bce3f415
1303575952@qq.com <1303575952@qq.com>
2019-05-16 11:00:19 +0800

    二叉树中和为某一值的路径

---
+++ src/no24_find_path/TreeNode.java
@@ -0,0 +1,11 @@
+package no24_find_path;
+
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
