5bcb69290bc606914ac3e6147cf6fabcd841286c
1303575952@qq.com <1303575952@qq.com>
2019-06-03 14:40:11 +0800

    重建二叉树

--- src/no4_rebuild_by_pre_and_in/Solution.java
+++ src/no4_rebuild_by_pre_and_in/Solution.java
@@ -7,7 +7,7 @@ package no4_rebuild_by_pre_and_in;
  */
 public class Solution {
     public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
-        return reConstructBinaryTree(pre, 0, pre.length-1, in, 0, in.length-1);
+        return reConstructBinaryTree(pre, 0, pre.length - 1, in, 0, in.length - 1);
     }
 
     public TreeNode reConstructBinaryTree(int[] pre, int preStart, int preEnd, int[] in, int inStart, int inEnd) {
@@ -24,7 +24,25 @@ public class Solution {
         return treeNode;
     }
 
-    public static void main(String[] args) {
+    /**
+     * 后序遍历
+     *
+     * @param treeNode
+     */
+    private static void post(TreeNode treeNode) {
+        if (treeNode.left != null) {
+            post(treeNode.left);
+        }
+        if (treeNode.right != null) {
+            post(treeNode.right);
+        }
+        System.out.print(treeNode.val + " ");
+    }
 
+    public static void main(String[] args) {
+        int[] pre = {1, 2, 4, 7, 3, 5, 6, 8};
+        int[] in = {4, 7, 2, 1, 5, 3, 8, 6};
+        TreeNode treeNode = new Solution().reConstructBinaryTree(pre, in);
+        post(treeNode);
     }
 }
