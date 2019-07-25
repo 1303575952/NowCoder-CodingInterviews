04462218224d3193c6118c920925a2ebf5b35933
1303575952 <1303575952@qq.com>
2019-07-25 16:02:42 +0800

    不用加减乘除做加法

---
+++ src/no48_add/Solution.java
@@ -0,0 +1,19 @@
+package no48_add;
+
+/**
+ * 写一个函数，求两个整数之和，要求在函数体内不得使用+、-、*、/四则运算符号。
+ */
+public class Solution {
+    public int add(int num1, int num2) {
+        while (num2 != 0) {
+            int temp = num1 ^ num2;
+            num2 = (num1 & num2) << 1;
+            num1 = temp;
+        }
+        return num1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().add(123, 987));
+    }
+}
