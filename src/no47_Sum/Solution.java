f9fcb57fd2756e8f2538d404c1242c5b5cbe23f2
1303575952 <1303575952@qq.com>
2019-07-25 15:42:45 +0800

    求1+2+3+...+n

---
+++ src/no47_Sum/Solution.java
@@ -0,0 +1,16 @@
+package no47_Sum;
+
+/**
+ * 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ */
+public class Solution {
+    private int sum_Solution(int n) {
+        int sum = n;
+        boolean ans = (n > 0) && ((sum += sum_Solution(n - 1)) > 0);
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().sum_Solution(10));
+    }
+}
