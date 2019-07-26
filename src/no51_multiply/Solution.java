9f55461c3637d3c75d0c57fa27e364e1d9f3c3e1
1303575952 <1303575952@qq.com>
2019-07-26 11:11:58 +0800

    构建乘积数组

---
+++ src/no51_multiply/Solution.java
@@ -0,0 +1,34 @@
+package no51_multiply;
+
+/**
+ * 给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],
+ * 其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。不能使用除法。
+ */
+public class Solution {
+    private int[] multiply(int[] A) {
+        int length = A.length;
+        int[] B = new int[length];
+        if (length != 0) {
+            B[0] = 1;
+            //计算下三角连乘
+            for (int i = 1; i < length; i++) {
+                B[i] = B[i - 1] * A[i - 1];
+            }
+            int temp = 1;
+            //计算上三角
+            for (int j = length - 2; j >= 0; j--) {
+                temp *= A[j + 1];
+                B[j] *= temp;
+            }
+        }
+        return B;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] B = new Solution().multiply(A);
+        for (int i = 0; i < B.length; i++) {
+            System.out.println(B[i]);
+        }
+    }
+}
