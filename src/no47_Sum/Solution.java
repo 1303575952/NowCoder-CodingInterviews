4de201581831af2f6abbf178ce5c05b218db05f4
1303575952 <1303575952@qq.com>
2019-07-30 15:44:11 +0800

    rename method

--- src/no47_Sum/Solution.java
+++ src/no47_Sum/Solution.java
@@ -4,13 +4,13 @@ package no47_Sum;
  * 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
  */
 public class Solution {
-    private int sum_Solution(int n) {
+    private int sum(int n) {
         int sum = n;
-        boolean ans = (n > 0) && ((sum += sum_Solution(n - 1)) > 0);
+        boolean ans = (n > 0) && ((sum += sum(n - 1)) > 0);
         return sum;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().sum_Solution(10));
+        System.out.println(new Solution().sum(10));
     }
 }
