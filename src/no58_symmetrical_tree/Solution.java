6062bcf2eac9ef15d59c0634185884e75358509c
1303575952 <1303575952@qq.com>
2019-08-05 12:01:16 +0800

    对称的二叉树

---
+++ src/no58_symmetrical_tree/Solution.java
@@ -0,0 +1,17 @@
+package no58_symmetrical_tree;
+
+public class Solution {
+    boolean isSymmetrical(TreeNode pRoot) {
+        if (pRoot == null) {
+            return true;
+        }
+        return comRoot(pRoot.left, pRoot.right);
+    }
+
+    private boolean comRoot(TreeNode left, TreeNode right) {
+        if (left == null) return right == null;
+        if (right == null) return false;
+        if (left.val != right.val) return false;
+        return comRoot(left.right, right.left) && comRoot(left.left, right.right);
+    }
+}
---
+++ src/no58_symmetrical_tree/TreeNode.java
@@ -0,0 +1,12 @@
+package no58_symmetrical_tree;
+
+
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
