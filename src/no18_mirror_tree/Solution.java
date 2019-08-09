aa1518c7e2b2072b8bf2623cd830c83c6b4432d8
1303575952 <1303575952@qq.com>
2019-08-09 22:25:31 +0800

    二叉树的镜像题目描述

--- src/no18_mirror_tree/Solution.java
+++ src/no18_mirror_tree/Solution.java
@@ -1,18 +1,21 @@
 package no18_mirror_tree;
 
+/**
+ * 操作给定的二叉树，将其变换为源二叉树的镜像。
+ */
 public class Solution {
-	public void mirrorTree(TreeNode node) {
-		TreeNode temp;
-		if (node != null) {
-			temp = node.left;
-			node.left = node.right;
-			node.right = temp;
-		}
-		if (node.left != null) {
-			mirrorTree(node.left);
-		}
-		if (node.right != null) {
-			mirrorTree(node.right);
-		}
-	}
+    public void mirrorTree(TreeNode node) {
+        TreeNode temp;
+        if (node != null) {
+            temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+        }
+        if (node.left != null) {
+            mirrorTree(node.left);
+        }
+        if (node.right != null) {
+            mirrorTree(node.right);
+        }
+    }
 }
