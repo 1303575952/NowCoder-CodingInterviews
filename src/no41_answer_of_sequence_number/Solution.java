be99a623333eba5608ad12774d903e79595c4ac0
1303575952 <1303575952@qq.com>
2019-08-11 13:47:03 +0800

    和为S的连续正数序列

--- src/no41_answer_of_sequence_number/Solution.java
+++ src/no41_answer_of_sequence_number/Solution.java
@@ -7,26 +7,34 @@ import java.util.ArrayList;
  * 但是他并不满足于此,他在想究竟有多少种连续的正数序列的和为100(至少包括两个数)。
  * 没多久,他就得到另一组连续正数和为100的序列:18,19,20,21,22。
  * 现在把问题交给你,你能不能也很快的找出所有和为S的连续正数序列? Good Luck!
+ * <p>
+ * 双指针确定范围
  */
 public class Solution {
-	public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
-		ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
-		if (sum == 1)
-			return list;
-		int n = (int) (Math.ceil((Math.sqrt(8 * sum + 1) - 1) / 2));
-		int tmp = 0;
-		int num = 0;
-		for (int i = n; i > 1; i--) {
-			tmp = (i + 1) * i / 2;
-			if ((sum - tmp) % i == 0) {
-				ArrayList<Integer> arrayList = new ArrayList<Integer>();
-				num = (sum - tmp) / i;
-				for (int a = 1; a <= i; a++) {
-					arrayList.add(num + a);
-				}
-				list.add(arrayList);
-			}
-		}
-		return list;
-	}
+    public ArrayList<ArrayList<Integer>> findContinuousSequence(int sum) {
+        //存放结果
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        //两个起点，相当于动态窗口的两边，根据其窗口内的值的和来确定窗口的位置和大小
+        int plow = 1, phigh = 2;
+        while (phigh > plow) {
+            //由于是连续的，差为1的一个序列，那么求和公式是(a0+an)*n/2
+            int cur = (phigh + plow) * (phigh - plow + 1) / 2;
+            //相等，那么就将窗口范围的所有数添加进结果集
+            if (cur == sum) {
+                ArrayList<Integer> list = new ArrayList<>();
+                for (int i = plow; i <= phigh; i++) {
+                    list.add(i);
+                }
+                result.add(list);
+                plow++;
+                //如果当前窗口内的值之和小于sum，那么右边窗口右移一下
+            } else if (cur < sum) {
+                phigh++;
+            } else {
+                //如果当前窗口内的值之和大于sum，那么左边窗口右移一下
+                plow++;
+            }
+        }
+        return result;
+    }
 }
